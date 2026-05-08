4614fd32fafbba79f70fc49e1e92bea208382649
felix10339 <chatuejorda@gmail.com>
2026-05-08 15:49:34 +0100

    Fix API URL config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || '`${API_URL}`';
+const API_URL =process.env.REACT_APP_API_URL ||
+  'https://industrious-unity-production-895b.up.railway.app';
+
 export default API_URL;
